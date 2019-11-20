9d7eeade1f1f6e9364d26333cdd78b9b79c1f122
PengYD <1162347920@qq.com>
2019-11-20 12:51:50 +0800

    111

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <title>哔哩哔哩 (゜-゜)つロ 干杯~-bilibili</title>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.bilibili.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Bilibili
         </a>
       </header>
     </div>
